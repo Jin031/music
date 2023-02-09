a96faeaa3ddd12d90186f44426af2e05a86a42ae
Jin031 <Jin292941@gmail.com>
2023-02-09 13:30:22 +0000

    [Add] music

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,29 +1,23 @@
 class Admin::CommentsController < ApplicationController
- before_action :authenticate_admin!
- def index
-
-  ngword = ["キモイ","バカ"]
-
-  @comments = []
-  ngword.each do |keyword|  # 分割したキーワードごとに検索
-   @comments += Comment.where('comment LIKE(?)', "%#{keyword}%") # 部分一致で検索
+  before_action :authenticate_admin!
+  def index
+    ngword = ["キモイ","バカ"]
+    @comments = []
+      ngword.each do |keyword|  # 分割したキーワードごとに検索
+        @comments += Comment.where('comment LIKE(?)', "%#{keyword}%") # 部分一致で検索
+      end
   end
- end
 
-# def ng(text)
-#   ngword.any?{|t|text.include?(t)}
-# end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to admin_comments_path, notice: "コメントを削除しました"
+  end
 
- def destroy
-  @comment = Comment.find(params[:id])
-  @comment.destroy
-  redirect_to admin_comments_path, notice: "コメントを削除しました"
- end
+  private
 
- private
-
- def comment_params
-  params.require(:comment).permit(:customer_id, :post_id, :comment)
- end
+  def comment_params
+    params.require(:comment).permit(:customer_id, :post_id, :comment)
+  end
 end
 
