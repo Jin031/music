24a878073ce51de4f7d08430a37604703eff777c
Jin031 <Jin292941@gmail.com>
2023-02-11 12:59:01 +0000

    [Add] music

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_11_124812) do
+ActiveRecord::Schema.define(version: 2022_12_12_163307) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -122,7 +122,6 @@ ActiveRecord::Schema.define(version: 2023_02_11_124812) do
     t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "genre_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
